3a3d560170eae891471030cdc565df10f83ed9f0
kam <kam>
2023-08-06 18:06:01 +0000

    Update reset password

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('register/', views.SignUpView.as_view(), name='register'),
-    path('reset_password/', views.CustomPasswordResetView.as_view(), name='reset_password'),
+    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('custom_password_reset_confirm', views.CustomPasswordResetConfirmView.as_view(), name='custom_password_reset_confirm'),
-
-
+    path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path("reset/done/", views.CustomPasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("", include("django.contrib.auth.urls")),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, \
-    PasswordResetConfirmView
+    PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import SignUpForm
@@ -17,14 +17,12 @@ class CustomLoginView(LoginView):
         return self.render_to_response(self.get_context_data(form=form))
 
 
-
-# Sign Up View
 class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
 
-# Reset Password View
+
 class CustomPasswordResetView(PasswordResetView):
     success_url = reverse_lazy("password_reset_done")
     template_name = 'registration/custom_password_reset_form.html'
@@ -34,7 +32,14 @@ class CustomPasswordResetView(PasswordResetView):
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     template_name = "registration/custom_password_reset_done.html"
 
+
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     success_url = reverse_lazy("custom_password_reset_complete")
     template_name = "registration/custom_password_reset_confirm.html"
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    pass
+    #template_name = "registration/my_password_reset_complete.html"
+
 
