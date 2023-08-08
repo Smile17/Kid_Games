30961ec7c621ba638d0d77c44526b38c38ab28d1
kam <kam>
2023-08-08 11:13:39 +0000

    Update libraries

--- cards_gallery/models.py
+++ cards_gallery/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 from django_resized import ResizedImageField
 
 
@@ -31,7 +32,10 @@ class Card(models.Model):
     def __str__(self):
         return self.title
 
-
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
 
 class CardItem(models.Model):
--- content_maker/Content_Generator/coloring_letters.py
+++ content_maker/Content_Generator/coloring_letters.py
@@ -1,41 +1,87 @@
+import PIL
 from PIL import Image, ImageDraw, ImageFont
-import os, glob
-import numpy as np
 import random
+import PIL.ImageOps
 
-parentPath = "CaptchaDataset\\"
+#A4_DIMS = (2480, 3508)
+A4_DIMS = (2400, 3400)
+#A4_DIMS = (2174, 3075)
+texts = [u"КА", u"КО", u"ЖУ"]
 
-charactersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                  'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
-                  '8', '9']
-fontTypes = []
+def outline_text(dr, pos, text, fnt, stroke, fill, stroke_width=6):
+    "Draw outline-style text"
+    dr.text((pos[0] - 1, pos[1]), text, font=fnt, fill=stroke, stroke_width=stroke_width)
+    dr.text((pos[0] + 1, pos[1]), text, font=fnt, fill=stroke, stroke_width=stroke_width)
+    dr.text((pos[0], pos[1] - 1), text, font=fnt, fill=stroke, stroke_width=stroke_width)
+    dr.text((pos[0], pos[1] + 1), text, font=fnt, fill=stroke, stroke_width=stroke_width)
+    dr.text(tuple(pos), text, font=fnt, fill=fill)
 
-for font in glob.glob("fonts\\*"):  # Create Image for every font
-    fontTypes.append(os.path.abspath(font))
+    #draw.text(digit_offset, ch, fill='white', font=font, stroke_width=2, stroke_fill='black')
 
-font = ImageFont.load_default()
-fontTypes.append(font)
+def draw_coloring(texts):
+    YC_WIDTH = A4_DIMS[0]
+    YC_HEIGHT = A4_DIMS[1]
+    FRAME_WIDTH = 200
+    YC_BACKCOLOR = (255, 255, 255, 255)
+    YC_BACKCOLOR_TR = (255, 255, 255, 0)
+    YC_TEXTCOLOR = (0, 0, 0, 255)
+    ROW_HEIGHT = 450
+    WAVE_LENGTH = 100
+    BOTTOM_PLACE = 400
 
-for index, character in enumerate(charactersList):
-    path = os.path.join(parentPath, character + "_" + str(index))
+    img = Image.new('RGBA', (YC_WIDTH, YC_HEIGHT), color=YC_BACKCOLOR)
 
-    if not os.path.exists(path):
-        os.mkdir(path)
+    digit_sz = [0, 0]
+    digit_offset = [FRAME_WIDTH, FRAME_WIDTH]
+    row = 0
+    b = False
+    while True:
+        while True:
+            digit = random.choice(texts)
+            font = ImageFont.truetype("Roboto-Regular.ttf", size=random.randint(100, 400), encoding="utf-8")
+            txt = Image.new('RGBA', img.size, YC_BACKCOLOR_TR)
+            draw = ImageDraw.Draw(txt)
+            new_box = draw.textbbox((0, 0), digit, font)
+            w = new_box[2] - new_box[0]  # bottom-top
+            h = new_box[3] - new_box[1]  # right-left
+            digit_sz = w, h
+            digit_offset[0] += random.randint(0, WAVE_LENGTH)
+            digit_offset[1] = FRAME_WIDTH + ROW_HEIGHT * row + random.randint(0, WAVE_LENGTH)
 
-    for imageCounter in range(len(fontTypes)):
-        print(imageCounter)
-        for repeats in range(3):  # Number of Images
-            # img = Image.new('1', (28, 28), color = 'black')
-            fnt = ImageFont.truetype(fontTypes[imageCounter], random.randint(30, 50))
-            w, h = fnt.getsize(character)
-            img_w, img_h = w + 20, h + 20  # Add 20 pixels padding (assume 10 pixels from each side).
-            img = Image.new('L', (img_w, img_h),
-                            color='black')  # Replace '1' with 'L' (8-bit pixels, black and white - we fill 255 so we can't use 1 bit per pixel)
-            d = ImageDraw.Draw(img)
-            d.text(((img_w - w) / 2, (img_h - h) / 2), character, font=fnt, fill=255,
-                   align="center")  # TO ALIGN CHARACTER IN CENTER
-            # img = np.pad(img, pad_width=10, mode='constant', constant_values=0) #Manually added padding
-            # img = Image.fromarray(img)
-            img.save(path + "\\" + str(imageCounter) + "_" + str(repeats) + ".jpg")
-        break
+            if digit_offset[0] + w > YC_WIDTH - FRAME_WIDTH:
+                break
+            if digit_offset[1] + h > YC_HEIGHT - FRAME_WIDTH - BOTTOM_PLACE:
+                b = True
+                break
+            print(digit_offset)
+            #draw.line([(digit_offset[0] - 20, digit_offset[1] - 20), (digit_offset[0], digit_offset[1])],
+            #          fill='blue', width=2, joint='curve')  # Horizontal
+            outline_text(draw, digit_offset, digit, font, YC_TEXTCOLOR, YC_BACKCOLOR_TR)
+            digit_offset[0] += digit_sz[0]
+
+            img = Image.alpha_composite(img, txt)
+            #img.show()
+        if b:
+            break
+        digit_offset[0] = FRAME_WIDTH + random.randint(0, int(WAVE_LENGTH / 5))
+        row += 1
+        print(row)
+
+    return img
+
+def draw_hero(xy, img, hero):
+    hero = hero.convert('L')
+    hero = hero.point(lambda p: 255 if p > 200 else 0)
+    mask = PIL.ImageOps.invert(hero)
+    img.paste(hero, xy, mask)
+    return img
+
+if __name__ == "__main__":
+    im = draw_coloring(texts)
+    hero = Image.open('baby_shark.png')
+    YC_WIDTH = A4_DIMS[0]
+    YC_HEIGHT = A4_DIMS[1]
+    xy = (YC_WIDTH - hero.size[0] - 50, YC_HEIGHT - hero.size[1] - 50)
+    im = draw_hero(xy, im, hero)
+    im.show()
+    im.save("res.png")
